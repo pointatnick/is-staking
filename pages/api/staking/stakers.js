95c72ec6623bbfc3f11d6533814e2fe9dd10ff8f
pointatnick <nicholashma@gmail.com>
2022-02-01 15:19:03 -0800

    add stakers route

--- pages/api/staking/stakers.js
+++ pages/api/staking/stakers.js
@@ -1,25 +1,26 @@
 import { PublicKey } from '@solana/web3.js';
-import { connection, DAO_PUBLIC_KEY } from '../../../src/config';
+import { CONNECTION, DAO_PUBLIC_KEY } from '../../../src/config';
+import { getSerpentsFromWallet } from '../serpents/[publicKey]';
 
 export default async function handler(req, res) {
-  // const diamonds = await getDiamondsFromWallet(DAO_PUBLIC_KEY);
-  // // get most recent signature of diamonds in DAO
-  // const signatures = await Promise.all(
-  //   diamonds.map(async (mint) => {
-  //     // get most recent signature
-  //     return (await connection.getSignaturesForAddress(new PublicKey(mint)))[0]
-  //       .signature;
-  //   })
-  // );
-  // // get mints that are staked that belong to the connected wallet
-  // const stakedMints = await Promise.all(
-  //   signatures.map(async (signature) => {
-  //     const tx = await connection.getConfirmedTransaction(
-  //       signature,
-  //       'confirmed'
-  //     );
-  //     return tx.transaction.feePayer.toString();
-  //   })
-  // );
-  // res.status(200).json(stakedMints);
+  const serpents = await getSerpentsFromWallet(DAO_PUBLIC_KEY);
+  // get most recent signature of diamonds in DAO
+  const signatures = await Promise.all(
+    serpents.map(async (mint) => {
+      // get most recent signature
+      return (await CONNECTION.getSignaturesForAddress(new PublicKey(mint)))[0]
+        .signature;
+    })
+  );
+  // get mints that are staked that belong to the connected wallet
+  const stakedMints = await Promise.all(
+    signatures.map(async (signature) => {
+      const tx = await CONNECTION.getConfirmedTransaction(
+        signature,
+        'confirmed'
+      );
+      return tx.transaction.feePayer.toString();
+    })
+  );
+  res.status(200).json(stakedMints);
 }
